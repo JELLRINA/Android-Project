dbd3c048829b93d428353c7f780ce351e4de5953
jellrina <dlgh6007@gmail.com>
2021-12-12 04:47:14 +0900

    시간 및 날짜 예약 설정

---
+++ app/src/main/java/com/example/reservationapp/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.reservationapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+    //전역변수 부여
+    Chronometer chrono;
+    Button btnStart, btnEnd;
+    RadioButton rdoCal, rdoTime;
+    CalendarView calView;
+    TimePicker tPicker;
+    TextView tvYear, tvMonth, tvDay, tvHour, tvMinute;
+    int selectYear, selectMonth, selectDay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+
+        setTitle("시간 예약");
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnEnd = (Button) findViewById(R.id.btnEnd);
+
+        chrono = (Chronometer) findViewById(R.id.chronometer1);
+
+        rdoCal = (RadioButton) findViewById(R.id.rdoCal);
+        rdoTime = (RadioButton) findViewById(R.id.rdoTime);
+
+        calView = (CalendarView) findViewById(R.id.calenderView1);
+        tPicker = (TimePicker) findViewById(R.id.timePIcker1);
+
+        tvYear = (TextView) findViewById(R.id.tvYear);
+        tvMonth = (TextView) findViewById(R.id.tvMonth);
+        tvDay = (TextView) findViewById(R.id.tvDay);
+        tvHour = (TextView) findViewById(R.id.tvHour);
+        tvMinute = (TextView) findViewById(R.id.tvMinute);
+
+        calView.setVisibility(View.INVISIBLE);
+        tPicker.setVisibility(View.INVISIBLE);
+
+        rdoCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tPicker.setVisibility(View.INVISIBLE);
+                calView.setVisibility(View.VISIBLE); //날짜설정(캘린더뷰) 클릭 시 캘린더만 뜨기
+            }
+        });
+
+        rdoTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tPicker.setVisibility(View.VISIBLE); //날짜설정(캘린더뷰) 클릭 시 시간만 뜨기
+                calView.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chrono.setBase(SystemClock.elapsedRealtime()); //0으로 초기화
+                chrono.start(); //예약시작
+                chrono.setTextColor(Color.RED);
+            }
+        });
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chrono.stop(); //예약완료 정지
+                chrono.setTextColor(Color.BLUE);
+
+                tvYear.setText(Integer.toString(selectYear));
+                tvMonth.setText(Integer.toString(selectMonth));
+                tvDay.setText(Integer.toString(selectDay)); //년,월,일 텍스트뷰에 채움
+
+                tvHour.setText(Integer.toString(tPicker.getCurrentHour()));
+                tvMinute.setText(Integer.toString(tPicker.getCurrentMinute())); //시간설정에서 시,분을 텍스트뷰에 채움
+            }
+        });
+        calView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                selectYear = year;
+                selectMonth = month+1; //0월부터 시작이므로 +1
+                selectDay = dayOfMonth;
+            }
+        });
+
+    }
+}
